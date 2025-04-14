1b7ab319bdffee1126c8162e53e50ef5f3f2e464
snooptodd <snooptodd@gmail.com>
2025-04-13 22:00:36 -0400

    what was done in manyuglypages.php merged into ALLJson2Graphhtml.py

--- ALLJson2Graphhtml.py
+++ ALLJson2Graphhtml.py
@@ -1,38 +1,45 @@
-## create graphs for the live and pts json files 
+## create graphs and pages for the live and pts json files 
 
-# get list of files in live/json, live/jsonalt, pts/json, pts/jsonalt
-# loop through each list creating a graph including the matches from the other lists
-# super inefficent but pyplot hopefully dosent overwrite by default. 
+# get list of files in live, pts
+# loop through list creating a graph and page including the matching json file from the live/pts,json/jsonalt dirs
+# super inefficent but it works.
 
 ###### make some test files in the json folders 
-# probalem for future me
+# they are there "just" need to write up the test
 
 import os
-import subprocess
-import shutil
 import pathlib
-import filecmp
 import json
 import matplotlib.pyplot as plt
 
 DEBUG=False
-RootDir="./json"
-#livePatch="/PTS_10OCT24"
-LIVEDIR="/Live_P58"
-PTSDIR="/PTS_P60_4apr"
-CommonDIR='/misc/curvetables'
-SearchName="*.json"
+ROOT_DIR="./json"
+LIVE_DIR="/Live_P58_7apr"
+PTS_DIR="/PTS_P60_11apr"
+COMMON_DIR='/misc/curvetables'
+SEARCH_NAME="*.json"
 PTSDIRList=[]
 LIVEDIRList=[]
+PTSJsonaltPath = PTSJsonPath = PTSPath = PTSDIRList = LiveJsonaltPath = LiveJsonPath = LivePath = LiveDIRList = file = file1 = file2 = file3 = file4 = indexhtml = ""
+all=set(())
 
 if DEBUG :
-    LIVEDIR="/test_live"
-    PTSDIR="/test_pts"
+    LIVE_DIR="/test_live"
+    PTS_DIR="/test_pts"
 
-# Specify the directory path you want to start from
+def htmlheader(pagename):
+    return f'''<!DOCTYPE html> <html lang="en"> 
+  <head> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> 	
+  <title>{pagename}</title> 
+  <link rel="stylesheet" href="/76CurveTables/styles.css"> </head> <body>'''
 
-# PTSDIRList=list_files_recursive(RootDir+PTSDIR)
-# LIVEDIRList=list_files_recursive(RootDir+LIVEDIR+CommonDIR)
+def htmlfooter():
+    return '</body> </html>'
+
+def writefile(nameandpath, data):
+    f = open(nameandpath, "w")
+    f.write(data)
+    f.close()
 
 def makegraph():
     file1X=[]
@@ -43,30 +50,33 @@ def makegraph():
     file3Y=[]
     file4X=[]
     file4Y=[]
-    # file1=PTSfile
-    # file2=LiveFile
+    title = pathtmp = subpagehtml = ''
+    file1subpagehtml = file2subpagehtml = file3subpagehtml = file4subpagehtml = ''
+    file1txtdata = file2txtdata = file3txtdata = file4txtdata = ''
+    global indexhtml
 
     if DEBUG :
         print("DEBUG\n%s\n%s\n%s\n%s\n" % (file1, file2, file3, file4))
         return
     
     # Load JSON data from a file and extract the x and y data
-    
+        
     if file1 != '':
         title=file1.stem
-        searchpath = RootDir+PTSDIR+CommonDIR+'/jsonalt/'
-        pathtmp = str(file1.parent)[str(file1.parent).find('/jsonalt')+8:]   
+        pathtmp = str(file1.parent)[str(file1.parent).find('/jsonalt')+8:]
         with open(file1, 'r') as file:
             file1_json_data = json.load(file)
             file.close
         for entry in file1_json_data["curve"]:
             # print(entry)
+            file1txtdata+=f'x = {entry["x"]}, y = {entry["y"]}<br>'
             file1X.append(entry["x"])
             file1Y.append(entry["y"])
-        plt.plot(file1X,file1Y, label=str(PTSDIR)[1:]+' jsonAlt'+pathtmp, marker = 'o', markevery=0.1 )
+        plt.plot(file1X,file1Y, label=str(PTS_DIR)[1:]+' jsonAlt', marker = 'o', markevery=0.1 )
         for key,value in enumerate(file1X):
             if not value%10:
                 plt.annotate('(' + str(file1X[key]) + ', ' + str(file1Y[key]) + ')',(file1X[key], file1Y[key]))
+        file1subpagehtml+=f'<label for="ptsjsonalt">{file1}</label> <div class="box" id="ptsjsonalt">{file1txtdata}</div>'
 
     if file2 != '':
         title=(file2.stem)
@@ -76,12 +86,14 @@ def makegraph():
             file.close
         for entry in file2_json_data["curve"]:
             # print(entry)
+            file2txtdata+=f'x = {entry["x"]}, y = {entry["y"]}<br>'
             file2X.append(entry["x"])
             file2Y.append(entry["y"])
-        plt.plot(file2X,file2Y, label=str(PTSDIR)[1:]+" json"+pathtmp, marker = '*', markevery=0.1)
+        plt.plot(file2X,file2Y, label=str(PTS_DIR)[1:]+" json", marker = '*', markevery=0.1)
         for key,value in enumerate(file2X):
             if not value%10:
                 plt.annotate('(' + str(file2X[key]) + ', ' + str(file2Y[key]) + ')',(file2X[key], file2Y[key]))
+        file2subpagehtml+=f'<label for="ptsjson">{file2}</label> <div class="box" id="ptsjson">{file2txtdata}</div>'
 
     if file3 != '':
         title=(file3.stem)
@@ -91,12 +103,14 @@ def makegraph():
             file.close
         for entry in file3_json_data["curve"]:
             # print(entry)
+            file3txtdata+=f'x = {entry["x"]}, y = {entry["y"]}<br>'
             file3X.append(entry["x"])
             file3Y.append(entry["y"])
-        plt.plot(file3X,file3Y, label=str(LIVEDIR)[1:]+" jsonAlt"+pathtmp, marker = 'x', markevery=0.1)
+        plt.plot(file3X,file3Y, label=str(LIVE_DIR)[1:]+" jsonAlt", marker = 'x', markevery=0.1)
         for key,value in enumerate(file3X):
             if not value%10:
                 plt.annotate('(' + str(file3X[key]) + ', ' + str(file3Y[key]) + ')',(file3X[key], file3Y[key]))
+        file3subpagehtml+=f'<label for="Livejsonalt">{file3}</label> <div class="box" id="Livejsonalt">{file3txtdata}</div>'
 
     if file4 != '':
         title=(file4.stem)
@@ -106,12 +120,14 @@ def makegraph():
             file.close
         for entry in file4_json_data["curve"]:
             # print(entry)
+            file4txtdata+=f'x = {entry["x"]}, y = {entry["y"]}<br>'
             file4X.append(entry["x"])
             file4Y.append(entry["y"]) 
-        plt.plot(file4X,file4Y, label=str(LIVEDIR)[1:]+" json"+pathtmp, marker = '+', markevery=0.1)   
+        plt.plot(file4X,file4Y, label=str(LIVE_DIR)[1:]+" json", marker = '+', markevery=0.1)   
         for key,value in enumerate(file4X):
             if not value%10:
                 plt.annotate('(' + str(file4X[key]) + ', ' + str(file4Y[key]) + ')',(file4X[key], file4Y[key]))
+        file4subpagehtml+=f'<label for="Livejson">{file4}</label> <div class="box" id="Livejson">{file4txtdata}</div>'
 
     # Plotting
     xlabel="X"
@@ -127,46 +143,50 @@ def makegraph():
     plt.legend()
     plt.savefig(savePath+saveName)
     plt.close()
-
+    subpagehtml+=f'''{htmlheader(title)}
+<img src="{saveName}"><p>
+{file1subpagehtml}
+{file2subpagehtml}<br>
+{file3subpagehtml}
+{file4subpagehtml}<br>
+{htmlfooter()}'''
+    writefile(savePath+title+'.html', subpagehtml)
+    indexhtml+=f'<a href="{savePath+title+'.html'}">{title}</a><p>'
     return 
 
 
 # # get list of json files in dirs
 
-PTSJsonaltPath = pathlib.Path(RootDir+PTSDIR+CommonDIR+"/jsonalt")
-PTSJsonPath = pathlib.Path(RootDir+PTSDIR+CommonDIR+"/json")
-# PTSJsonaltDIRList = list(PTSJsonaltPath.rglob(SearchName))
-PTSPath = pathlib.Path(RootDir+PTSDIR+CommonDIR)
-PTSDIRList = list(PTSPath.rglob(SearchName))
-LiveJsonaltPath = pathlib.Path(RootDir+LIVEDIR+CommonDIR+"/jsonalt")
-LiveJsonPath = pathlib.Path(RootDir+LIVEDIR+CommonDIR+"/json")
-# LiveJsonaltDIRList = list(LiveJsonaltPath.rglob(SearchName))
-LivePath = pathlib.Path(RootDir+LIVEDIR+CommonDIR)
-LiveDIRList = list(LivePath.rglob(SearchName))
-file=""
-all=set(())
+PTSJsonaltPath = pathlib.Path(ROOT_DIR+PTS_DIR+COMMON_DIR+"/jsonalt")
+PTSJsonPath = pathlib.Path(ROOT_DIR+PTS_DIR+COMMON_DIR+"/json")
+PTSPath = pathlib.Path(ROOT_DIR+PTS_DIR+COMMON_DIR)
+PTSDIRList = list(PTSPath.rglob(SEARCH_NAME))
+LiveJsonaltPath = pathlib.Path(ROOT_DIR+LIVE_DIR+COMMON_DIR+"/jsonalt")
+LiveJsonPath = pathlib.Path(ROOT_DIR+LIVE_DIR+COMMON_DIR+"/json")
+LivePath = pathlib.Path(ROOT_DIR+LIVE_DIR+COMMON_DIR)
+LiveDIRList = list(LivePath.rglob(SEARCH_NAME))
 # loop through PTSDIRList. sets do not allow duplicates. each fiile will only be listed one time.
 # 
 for file in PTSDIRList:
     all.add(file.name)
 for file in LiveDIRList:
     all.add(file.name)
-
+alllist = list(all)
+alllist.sort()
 print(len(all))
+indexhtml=htmlheader('Curve Table Graphs')
 
-for name in all:
+for name in alllist:
     file1 = file2 = file3 = file4 = file = ''
     for file in PTSJsonaltPath.rglob(name):
         file1 = file
-    file = ''
     for file in PTSJsonPath.rglob(name):
         file2 = file
-    file = ''
     for file in LiveJsonaltPath.rglob(name):
         file3 = file
-    file = ''
     for file in LiveJsonPath.rglob(name):
         file4 = file
-    file = ''
-
     makegraph()
+
+indexhtml+=htmlfooter()
+writefile('./index.html',indexhtml)

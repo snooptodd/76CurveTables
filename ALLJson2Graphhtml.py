aacdec706e59d4172dcdc8d0133f7f323ded4c07
snooptodd <snooptodd@gmail.com>
2025-10-06 18:42:20 -0400

    PTS 64.01 6oct

--- ALLJson2Graphhtml.py
+++ ALLJson2Graphhtml.py
@@ -21,7 +21,7 @@ import matplotlib.pyplot as plt
 DEBUG=False
 ROOT_DIR="./json"
 LIVE_DIR="/Live_P62.1"
-PTS_DIR="/PTS_P64.0_2Oct"
+PTS_DIR="/PTS_P64.01_6Oct"
 COMMON_DIR='/misc/curvetables'
 SEARCH_NAME="*.json"
 PTSDIRList=[]
